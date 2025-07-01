c1415b143205a4d3c8ba1b03a0aa9877424cd0b9
CarltonDavis615 <carltondavis615@gmail.com>
2025-07-01 10:45:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("quoteForm").addEventListener("submit", async function(e
     service: e.target.service.value
   };
 
-await fetch("http://localhost:5678/webhook-test/quote-request", {
+await fetch("http://localhost:5678/webhook-test/46d90e4d-0b01-4c65-a4d1-b2d3cfb9d25d", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
